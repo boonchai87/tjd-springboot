eb25d437839be59300de950a8a29d5a34b1467cd
Boonchai Wedwanitsatheiin <neng87@gmail.com>
2021-12-08 17:32:19 +0700

    fixed credential in application.yml

--- src/main/java/com/thaiitwork/controller/HomeController.java
+++ src/main/java/com/thaiitwork/controller/HomeController.java
@@ -1,13 +1,18 @@
 package com.thaiitwork.controller;
 
+import com.thaiitwork.repository.TopicRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
+    @Autowired
+    private TopicRepository topicRepository;
 
     @GetMapping("/")
     public String index(){
+
         return "index";
     }
 
